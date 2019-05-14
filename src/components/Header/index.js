66dc212e3c8e263db999c90fa1d59935c9b84a89
Michael Douglas <maicondouglaspaim94@gmail.com>
2019-05-14 17:33:33 -0300

    Construção da interface

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css"
+
+const Header = () => <header id="main-header">
+<div className="header">
+    <h1>Ivory Store</h1>    
+</div>
+<div className='actions'>
+    <button>login</button>       
+</div>
+</header>;
+
+export default Header;
